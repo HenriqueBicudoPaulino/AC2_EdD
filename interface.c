9e0292fd863fa0499ffa92c74446d6872eb972a2
Matheus de Alcantara <matheus.alcantara.teixeira@gmail.com>
2025-05-05 21:44:29 -0300

    fix: formatação de texto na `interface.c` e `#include`s na `main.c`

--- interface.c
+++ interface.c
@@ -10,46 +10,55 @@ void menu(Lista *lista, Arvore *arvore) {
     int part_number, quantidade;
 
     do {
-        printf("\n1 - Buscar\n2 - Inserir\n3 - Utilizar\n4 - Descartar\n0 - Sair\nOpcao: ");
+        printf("========================================\n");
+		printf("                 MENU                   \n");
+		printf("========================================\n");
+		printf("1 - Buscar\n");
+		printf("2 - Inserir\n");
+		printf("3 - Utilizar\n");
+		printf("4 - Descartar\n");
+		printf("0 - Sair\n");
+		printf("Selecione uma opcao: ");
+
         scanf("%d", &opcao);
 
         switch (opcao) {
             case 1:
-                printf("Digite o Part Number: ");
+                printf("\nDigite o Part Number: ");
                 scanf("%d", &part_number);
 				
                 ini = clock();
                 NoLista *resultado_lista = busca_lista(lista, part_number);
                 fim = clock();
-                printf("Tempo Lista: %.5lfs\n", (double)(fim - ini) / CLOCKS_PER_SEC);
+                printf("\nTempo Lista: %.5lfs\n", (double)(fim - ini) / CLOCKS_PER_SEC);
                 if (resultado_lista && !fila_vazia(&resultado_lista->fila_lotes)) {
-                    printf("Item encontrado na lista:\n");
+                    printf("\nItem encontrado na lista:\n");
                     Item result_lista = resultado_lista->fila_lotes.inicio->dado;
                     imprime_item(result_lista);
                 } else {
-                    printf("Item não encontrado na lista ou fila vazia.\n");
+                    printf("\nItem nao encontrado na lista ou fila vazia.\n");
                 }
 
                 ini = clock();
                 NoArvore *resultado_arvore = busca_arvore(arvore, part_number);
                 fim = clock();
-                printf("Tempo Arvore: %.5lfs\n", (double)(fim - ini) / CLOCKS_PER_SEC);
+                printf("\nTempo Arvore: %.5lfs\n", (double)(fim - ini) / CLOCKS_PER_SEC);
                 if (resultado_arvore && !fila_vazia(&resultado_arvore->fila_lotes)) {
                     printf("Item encontrado na arvore:\n");
                     Item result_arvore = resultado_arvore->fila_lotes.inicio->dado;
                     imprime_item(result_arvore);
                 } else {
-                    printf("Item não encontrado na arvore ou fila vazia.\n");
+                    printf("Item nao encontrado na arvore ou fila vazia.\n");
                 }
                 break;
 
             case 2:
-                printf("Part Number: "); scanf("%d", &novo.part_number);
+                printf("\nPart Number: "); scanf("%d", &novo.part_number);
                 printf("Descricao: "); scanf("%s", novo.descricao);
                 do {
                     printf("Localizacao (Ex: AB1): "); scanf("%s", novo.localizacao);
                     if (!valida_localizacao(novo.localizacao)) {
-                        printf("Localizacao invalida. Tente novamente.\n");
+                        printf("\nLocalizacao invalida. Tente novamente.\n");
                     }
                 } while (!valida_localizacao(novo.localizacao));
                 printf("Shelf Life (meses): "); scanf("%d", &novo.shelf_life);
@@ -65,7 +74,7 @@ void menu(Lista *lista, Arvore *arvore) {
                 ini = clock();
                 insere_ordenado_lista(lista, novo.part_number, novo);
                 fim = clock();
-                printf("Inserido na Lista em %.5lfs\n", (double)(fim - ini) / CLOCKS_PER_SEC);
+                printf("\nInserido na Lista em %.5lfs\n", (double)(fim - ini) / CLOCKS_PER_SEC);
 
                 ini = clock();
                 insere_arvore(arvore, novo.part_number, novo);
@@ -74,13 +83,13 @@ void menu(Lista *lista, Arvore *arvore) {
                 break;
 
             case 3:
-                printf("Digite Part Number e Quantidade a Utilizar: ");
+                printf("\nDigite Part Number e Quantidade a Utilizar: ");
                 scanf("%d %d", &part_number, &quantidade);
 				
                 ini = clock();
                 utiliza_item_lista(lista, part_number, quantidade);
                 fim = clock();
-                printf("Utilizado na Lista em %.5lfs\n", (double)(fim - ini) / CLOCKS_PER_SEC);
+                printf("\nUtilizado na Lista em %.5lfs\n", (double)(fim - ini) / CLOCKS_PER_SEC);
 
                 ini = clock();
                 utiliza_item_arvore(arvore, part_number, quantidade);
@@ -89,13 +98,13 @@ void menu(Lista *lista, Arvore *arvore) {
                 break;
 
             case 4:
-                printf("Digite Part Number e Quantidade a Descartar: ");
+                printf("\nDigite Part Number e Quantidade a Descartar: ");
                 scanf("%d %d", &part_number, &quantidade);
 
                 ini = clock();
                 descarta_item_lista(lista, part_number, quantidade);
                 fim = clock();
-                printf("Descartado na Lista em %.5lfs\n", (double)(fim - ini) / CLOCKS_PER_SEC);
+                printf("\nDescartado na Lista em %.5lfs\n", (double)(fim - ini) / CLOCKS_PER_SEC);
 
                 ini = clock();
                 descarta_item_arvore(arvore, part_number, quantidade);
@@ -104,7 +113,7 @@ void menu(Lista *lista, Arvore *arvore) {
                 break;
 
             case 0:
-                printf("Encerrando...\n");
+                printf("\n\nEncerrando...\n\n");
                 break;
         }
     } while (opcao != 0);
