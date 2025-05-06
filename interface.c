dcb25cc84265b94ff0ffd280c6c22843ab373ed3
Matheus de Alcantara <matheus.alcantara.teixeira@gmail.com>
2025-05-05 21:03:04 -0300

    fix: modificada a opção 1 para exibir itens da lista e da arvore, com verificação de filas vazias e correções de digitação

--- interface.c
+++ interface.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include "interface.h"
+#include "item.h"  // Para imprime_item e valida_localizacao
 
 void menu(Lista *lista, Arvore *arvore) {
     int opcao;
@@ -17,43 +18,50 @@ void menu(Lista *lista, Arvore *arvore) {
                 printf("Digite o Part Number: ");
                 scanf("%d", &part_number);
 				
-				//dando errado
                 ini = clock();
-	                NoLista *resultado = busca_lista(lista, part_number);
-	                if (resultado) {
-				    printf("Item encontrado na lista:\n");
-				    Item result = resultado->fila_lotes.inicio->dado;
-				    imprime_item(result);
-					} else {
-					    printf("Item não encontrado na lista.\n");
-					}
+                NoLista *resultado_lista = busca_lista(lista, part_number);
                 fim = clock();
-                //dando errado
                 printf("Tempo Lista: %.5lfs\n", (double)(fim - ini) / CLOCKS_PER_SEC);
+                if (resultado_lista && !fila_vazia(&resultado_lista->fila_lotes)) {
+                    printf("Item encontrado na lista:\n");
+                    Item result_lista = resultado_lista->fila_lotes.inicio->dado;
+                    imprime_item(result_lista);
+                } else {
+                    printf("Item não encontrado na lista ou fila vazia.\n");
+                }
 
                 ini = clock();
-                busca_arvore(arvore, part_number);
+                NoArvore *resultado_arvore = busca_arvore(arvore, part_number);
                 fim = clock();
                 printf("Tempo Arvore: %.5lfs\n", (double)(fim - ini) / CLOCKS_PER_SEC);
+                if (resultado_arvore && !fila_vazia(&resultado_arvore->fila_lotes)) {
+                    printf("Item encontrado na arvore:\n");
+                    Item result_arvore = resultado_arvore->fila_lotes.inicio->dado;
+                    imprime_item(result_arvore);
+                } else {
+                    printf("Item não encontrado na arvore ou fila vazia.\n");
+                }
                 break;
 
             case 2:
                 printf("Part Number: "); scanf("%d", &novo.part_number);
                 printf("Descricao: "); scanf("%s", novo.descricao);
-                printf("Localizacao (Ex: AB1): "); scanf("%s", novo.localizacao);
-                printf("Shelf Life (dias): "); scanf("%d", &novo.shelf_life);
+                do {
+                    printf("Localizacao (Ex: AB1): "); scanf("%s", novo.localizacao);
+                    if (!valida_localizacao(novo.localizacao)) {
+                        printf("Localizacao invalida. Tente novamente.\n");
+                    }
+                } while (!valida_localizacao(novo.localizacao));
+                printf("Shelf Life (meses): "); scanf("%d", &novo.shelf_life);
                 printf("Flag (1=liberado / 0=retido): "); scanf("%d", &novo.flag);
                 printf("Quantidade: "); scanf("%d", &novo.quantidade);
 				
-				//data de entrada como data atual
+				// Data de entrada como data atual
 				time_t t = time(NULL);
 			    struct tm *data_atual = localtime(&t);
 			    snprintf(novo.data_entrada, sizeof(novo.data_entrada), "%02d/%02d/%04d",
              	data_atual->tm_mday, data_atual->tm_mon + 1, data_atual->tm_year + 1900);
              	
-             	//como a data inicial é a data atual o item não pode estar vencido
-             	 novo.flag = 1;
-             	 
                 ini = clock();
                 insere_ordenado_lista(lista, novo.part_number, novo);
                 fim = clock();
@@ -72,7 +80,7 @@ void menu(Lista *lista, Arvore *arvore) {
                 ini = clock();
                 utiliza_item_lista(lista, part_number, quantidade);
                 fim = clock();
-                printf("Utilizaado na Lista em %.5lfs\n", (double)(fim - ini) / CLOCKS_PER_SEC);
+                printf("Utilizado na Lista em %.5lfs\n", (double)(fim - ini) / CLOCKS_PER_SEC);
 
                 ini = clock();
                 utiliza_item_arvore(arvore, part_number, quantidade);
