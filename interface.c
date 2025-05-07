67f77219af35357780a5e42e10db6676cb2bc2fc
Henrique Bicudo Paulino <hbicudo.hb@gmail.com>
2025-05-07 20:10:39 -0300

    Atualizações do Interface.C
    Adicionado cabeçalho para cada operação além da limpeza da tela
    
    ao buscar, foram removidas as impressões duplicadas na tela, uma vez que o conteúdo da lista e da arvore é o mesmo.
    
    adicionado mais uma opção no menu principal para exibir o estoque
    
    Corrigido o insere, agora o campo descrição aceita espaços e o flag é definido automaticamente pelo sistema

--- interface.c
+++ interface.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include "interface.h"
 #include "item.h"
+
 
 void menu(Lista *lista, Arvore *arvore) {
     int opcao;
@@ -13,10 +16,11 @@ void menu(Lista *lista, Arvore *arvore) {
         printf("========================================\n");
 		printf("                 MENU                   \n");
 		printf("========================================\n");
-		printf("1 - Buscar\n");
-		printf("2 - Inserir\n");
-		printf("3 - Utilizar\n");
-		printf("4 - Descartar\n");
+		printf("1 - Exibir estoque\n");
+		printf("2 - Buscar\n");
+		printf("3 - Inserir\n");
+		printf("4 - Utilizar\n");
+		printf("5 - Descartar\n");
 		printf("0 - Sair\n");
 		printf("Selecione uma opcao: ");
 
@@ -25,7 +29,19 @@ void menu(Lista *lista, Arvore *arvore) {
 		NoArvore *resultado;
 		
         switch (opcao) {
-            case 1:
+        	case 1:
+        		system("cls");
+            	printf("========================================\n");
+				printf("                ESTOQUE                 \n");
+				printf("========================================\n");
+        		//codigo para exibir estoque
+        		break;
+            case 2:
+            	system("cls");
+            	printf("========================================\n");
+				printf("                BUSCAR                  \n");
+				printf("========================================\n");
+				
                 printf("\nDigite o Part Number: ");
                 scanf("%d", &part_number);
 				
@@ -33,39 +49,40 @@ void menu(Lista *lista, Arvore *arvore) {
                 NoLista *resultado_lista = busca_lista(lista, part_number);
                 fim = clock();
                 printf("\nTempo Lista: %.5lfs\n", (double)(fim - ini) / CLOCKS_PER_SEC);
-                if (resultado_lista && !fila_vazia(&resultado_lista->fila_lotes)) {
-                    printf("\nItem encontrado na lista:\n");
-                    Item result_lista = resultado_lista->fila_lotes.inicio->dado;
-                    imprime_item(result_lista);
-                } else {
-                    printf("\nItem nao encontrado na lista ou fila vazia.\n");
-                }
 
                 ini = clock();
                 NoArvore *resultado_arvore = busca_arvore(arvore, part_number);
                 fim = clock();
-                printf("\nTempo Arvore: %.5lfs\n", (double)(fim - ini) / CLOCKS_PER_SEC);
+                printf("Tempo Arvore: %.5lfs\n\n", (double)(fim - ini) / CLOCKS_PER_SEC);
+                
                 if (resultado_arvore && !fila_vazia(&resultado_arvore->fila_lotes)) {
-                    printf("Item encontrado na arvore:\n");
+                    printf("Item encontrado:\n");
                     Item result_arvore = resultado_arvore->fila_lotes.inicio->dado;
                     imprime_item(result_arvore);
                 } else {
-                    printf("Item nao encontrado na arvore ou fila vazia.\n");
+                    printf("Item nao encontrado ou fila vazia.\n");
                 }
                 break;
 
-            case 2:
-                printf("\nPart Number: "); scanf("%d", &novo.part_number);
-                printf("Descricao: "); scanf("%s", novo.descricao);
+            case 3:
+            	system("cls");
+            	printf("========================================\n");
+				printf("               INSERIR                  \n");
+				printf("========================================\n");
+				
+                printf("\nPart Number: "); scanf("%d", &novo.part_number); getchar();
+                printf("Descricao: "); fgets(novo.descricao, 100, stdin);
+                novo.descricao[strcspn(novo.descricao, "\n")] = '\0';
                 do {
-                    printf("Localizacao (Ex: AB1): "); scanf("%s", novo.localizacao);
+                    printf("Localizacao (Ex: AB1): "); fgets(novo.localizacao, 4, stdin);
                     if (!valida_localizacao(novo.localizacao)) {
                         printf("\nLocalizacao invalida. Tente novamente.\n");
                     }
                 } while (!valida_localizacao(novo.localizacao));
                 printf("Shelf Life (meses): "); scanf("%d", &novo.shelf_life);
-                printf("Flag (1=liberado / 0=retido): "); scanf("%d", &novo.flag);
                 printf("Quantidade: "); scanf("%d", &novo.quantidade);
+                
+				novo.flag = 1; // liberado
 				
 				// Data de entrada como data atual
 				time_t t = time(NULL);
@@ -84,7 +101,12 @@ void menu(Lista *lista, Arvore *arvore) {
                 printf("Inserido na Arvore em %.5lfs\n", (double)(fim - ini) / CLOCKS_PER_SEC);
                 break;
 
-            case 3:
+            case 4:
+            	system("cls");
+            	printf("========================================\n");
+				printf("               UTILIZAR                 \n");
+				printf("========================================\n");
+				
                 printf("\nDigite Part Number:");
                 scanf("%d", &part_number);
                 resultado = busca_arvore(arvore, part_number);
@@ -113,7 +135,11 @@ void menu(Lista *lista, Arvore *arvore) {
 				}
                 break;
 
-            case 4:
+            case 5:
+            	system("cls");
+            	printf("========================================\n");
+				printf("               DESCARTAR                \n");
+				printf("========================================\n");
                 printf("\nDigite o Part Number: ");
                 scanf("%d", &part_number);
                 resultado = busca_arvore(arvore, part_number);
